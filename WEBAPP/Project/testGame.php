5c3fa1ae1edd754fd25d9b498b224e3c7d96b40d
cosed429 <cosed429@school.lu>
2026-03-16 15:57:07 +0100

    commit

--- WEBAPP/Project/testGame.php
+++ WEBAPP/Project/testGame.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="Jquery.js"></script>
     <script src = "testGame.js"></script>
+    <link rel="stylesheet" href="style.css">
     <title>Tic Tac Toe</title>
 </head>
 <body>
