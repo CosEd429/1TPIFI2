fce71647471560fd02c895423cf9a0b6cba5fa22
cosed429 <cosed429@school.lu>
2026-03-23 09:54:27 +0100

    get exercises

--- WebApp Rattrapage/Exercises with php and html/Exercise 1/Client.js
+++ WebApp Rattrapage/Exercises with php and html/Exercise 1/Client.js
@@ -2,7 +2,7 @@ $(body)
 
 function body(){
 $("#load").on("click" , function(){
-    $.get("Service.php" , function(data){
+    $.get("Service.php" , { "Key1" : 1 , "Key2" : 2},function(data){
         $("#textChange").html(data)
     })
 })
--- WebApp Rattrapage/Exercises with php and html/Exercise 1/Service.php
+++ WebApp Rattrapage/Exercises with php and html/Exercise 1/Service.php
@@ -1,3 +1,12 @@
 <?php
-    echo "Hello world + test"
+    echo "Hello world + test </br>";
+
+    $Diff = ["EM" => 1 , "Parking Haute" => 2 , "Train Station" => 3];
+
+    $Diff["Auchan"] = 100;
+    foreach($_GET as $key => $value){
+        print($key. " ". $value . "</br>");
+    } 
+   
+
 ?>
